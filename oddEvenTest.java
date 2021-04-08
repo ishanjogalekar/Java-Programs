21f755f223a95376c7db107765fe4b2861400ed3
Ishan Jogalekar <67326067+ishanjogalekar@users.noreply.github.com>
2021-04-08 10:25:56 +0530

    Create oddEvenTest.java

---
+++ oddEvenTest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class OddEvenTest {
+    public static boolean isOdd(int number) {
+        if(number % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int n;
+        boolean r;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no:");
+        n = sc.nextInt();
+        r = isOdd(n);
+
+        if(r){
+            System.out.println(n +" is even number.");
+        }
+        else{
+            System.out.println(n +" is odd number.");
+        }
+        sc.close();
+    }
+}
