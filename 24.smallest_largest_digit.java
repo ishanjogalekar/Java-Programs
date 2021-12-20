575c4e54e0af30ff8fe7cd8f5445a2e5d55c6778
Ishan Jogalekar <67326067+ishanjogalekar@users.noreply.github.com>
2021-12-20 15:13:09 +0530

    smallest and largest digit in number
    Using Math Package

---
+++ 24.smallest_largest_digit.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/*
+Developed By Ishan
+Find smallest and largest digit in number using Math package
+ */
+public class smallest_largest_digit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int smallest = 9;
+        int largest = 0;
+        while(n != 0)
+        {
+            int r = n % 10;
+            largest = Math.max(r,largest);
+            smallest = Math.min(r, smallest);
+            n = n / 10;
+        }
+        System.out.println("Smallest no:"+smallest);
+        System.out.println("Largest no:"+largest);
+    }
+}
